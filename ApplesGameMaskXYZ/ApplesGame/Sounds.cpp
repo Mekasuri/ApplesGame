783e57ceea1c5cbf3fad20bb7f39f30be9d6d202
Danil <fursenkodanilka@icloud.com>
2025-04-14 20:43:10 +0300

    Revert "Added all game files"
    This reverts commit 6fd3d11e24f900a7b18a863afeed4cb0042983f0.

--- ApplesGameMaskXYZ/ApplesGame/Sounds.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sounds.h"
-#include "Settings.h"
-#include <cassert>
-namespace ApplesGame {
-	void SoundInitialization(Sound& sound, std::string soundName) {
-		assert(sound.buffer.loadFromFile(RESOURCES_PATH + soundName));
-		sound.sound.setBuffer(sound.buffer);
-	}
-}
